26155391316cd472f9046bc21bdd0c462a82c340
amal.shajan <amalshajan2011@gmail.com>
2023-03-30 08:49:40 +0530

    page basics

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Cards from './pages/Cards';
+
+function App() {
+    return (
+        <div className="App">
+            <Cards />
+        </div>
+    );
+}
+
+export default App;
